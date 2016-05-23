a8e352f49b113ac3f0c31a64e12e2b33d6901bd1
Amanda Daly and Wren Withers <Guest@epicodus-39.local>
2016-05-23 10:35:54 -0700

    Complete business logic w/ objects

--- Monday/journal/js/scripts.js
+++ Monday/journal/js/scripts.js
@@ -1,3 +1,13 @@
+//Business Logic
+function journalEntry(date, paragraph) {
+  this.date = date;
+  this.paragraph = paragraph;
+};
+
+
+
+
+//User Interface Logic
 $("form#entry").submit(function(event){
   event.preventDefault();
 
@@ -5,9 +15,9 @@ $("form#entry").submit(function(event){
   var userDate=$("input#Date").val();
   var userBody=$("input#Body").val();
 
+  var newEntry = new journalEntry(userDate, userBody);
 
-  $("#submitted-date").append(userDate);
-  $("#submitted-body").append(userBody);
+  $("ul#journal-entries").prepend("<li><h2>" + newEntry.date + "</h2><br><p>" + newEntry.paragraph + "</p></li>");
 
   $("input#Date").val("");
   $("input#Body").val("");
